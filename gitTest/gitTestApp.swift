5c4d9d707ffcbe6f5f697c8168b606b134c12414
Kame2021 <21cm0108@jec.ac.jp>
2022-10-31 23:57:04 +0900

    Initial Commit

---
+++ gitTest/gitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitTestApp.swift
+//  gitTest
+//
+//  Created by Ryo on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct gitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
